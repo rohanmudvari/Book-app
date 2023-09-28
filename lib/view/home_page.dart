7d0f37656c1e34d11841b12a92aac1f325307d1c
rohanmudvari <mudvari25@example.com>
2023-09-28 17:02:18 +0545

    changes

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,11 +1,52 @@
 import 'package:flutter/material.dart';
+
+
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold
-      (body: const Placeholder()
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+          appBar: AppBar(
+            toolbarHeight: 100,
+            flexibleSpace: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Watch Now', style: TextStyle(fontSize: 25),),
+                    IconButton(onPressed: (){}, icon: Icon(Icons.search, size: 27,))
+                  ],
+                ),
+              ),
+            ),
+            bottom: TabBar(
+
+                indicatorSize: TabBarIndicatorSize.tab,
+                // indicator: BoxDecoration(
+                //   color: Colors.red,
+                //   borderRadius: BorderRadius.circular(20)
+                // ),
+                tabs: [
+                  Tab(text: 'Popular'),
+                  Tab(text: 'Top Rated'),
+                  Tab(text:  'Upcoming',),
+                ]
+            ),
+          ),
+          body: TabBarView(
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                Text('hello world1'),
+                Text('hello world2'),
+                Text('hello world3'),
+              ]
+          )
+      ),
     );
   }
 }
